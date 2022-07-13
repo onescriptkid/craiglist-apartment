2bbcb10fd3c42678f2f5086a984d002865b95510
onescriptkid <onescriptkid@gmail.com>
2022-07-12 20:41:16 -0700

    Added all the real data, gifs, and assets

--- README.md
+++ README.md
@@ -3,10 +3,24 @@
 A quick and dirty diagram for my experience chasing down aparments on Craigslist
 
 
+![Sankey gif](./assets/sankey.gif)
+
+1
+<iframe
+  src="https://codepen.io/team/codepen/embed/preview/PNaGbb"
+  style="width:100%; height:300px;"
+></iframe>
+2
+<iframe
+  src="assets/sankey.gif"
+  style="width:100%; height:300px;"
+></iframe>
+3
 <iframe
   src="sankey.html"
   style="width:100%; height:300px;"
 ></iframe>
+4
 <iframe
   src="http://google.com"
   style="width:100%; height:300px;"
@@ -28,7 +42,7 @@ I used the following filters
 - bundle duplicates
 - lat/lng search via circular map selection
 
-## Email Template
+## [Email Template](https://support.google.com/a/users/answer/9308990?hl=en) 
 
 I used the following template, `email-template`, to solicit craiglist sellers for the following categories
 
@@ -36,10 +50,40 @@ I used the following template, `email-template`, to solicit craiglist sellers fo
 - apartments / housing for rent
 - sublets & temporary
 
+<br>
+
+![Do this to speed up emails](./assets/google_template.png)
+
 *email-template*
+<br>
 
 ```
+I'm a <career> looking for a move-in date of <early|mid|late> <month> and am interested in checking out the rental unit. If that sounds good to you, I'm ready to move forward with the process.
+
+
+
+Is there a good time for me to see the place in person? If so, what's the address?
+
+
+
+I live in the area with a flexible work schedule so anytime works for me, but weekends and after <time> on weekdays is preferable.
+
+
+
+Send an email or text if there's anything more you want to know about me.
+
+
+
+<FirstName LastInitial>
+
+
+
+Phone #: <phone #>
+
+Email: <email>
 ```
+
+
 
 ## Quickstart
 
@@ -47,4 +91,10 @@ I used the following template, `email-template`, to solicit craiglist sellers fo
 virtualenv -p python3 ./venv
 pip install -r requirements.txt
 python sankey.py
+```
+
+### Formatting and linting
+```
+black sankey.py
+flake8 sankey.py
 ```
--- sankey.py
+++ sankey.py
@@ -1,79 +1,80 @@
 import plotly.graph_objects as go
-import urllib, urllib.request, json
+import json
 
 # Original url
-# 'https://raw.githubusercontent.com/plotly/plotly.js/master/test/image/mocks/sankey_energy.json'
 with open("data.json", "r") as f:
     data = json.load(f)
-
-# Show json data
-import pprint
-pprint.pprint(data)
 
 # Configure plotly diagram
 # override gray link colors with 'source' colors
 opacity = 0.4
 # change 'magenta' to its 'rgba' value to add opacity
-data['data'][0]['node']['color'] = ['rgba(255,0,255, 0.8)' if color == "magenta" else color for color in data['data'][0]['node']['color']]
-data['data'][0]['link']['color'] = [data['data'][0]['node']['color'][src].replace("0.8", str(opacity))
-                                    for src in data['data'][0]['link']['source']]
+data["data"][0]["node"]["color"] = [
+    "rgba(255,0,255, 0.8)" if color == "magenta" else color
+    for color in data["data"][0]["node"]["color"]
+]
+data["data"][0]["link"]["color"] = [
+    data["data"][0]["node"]["color"][src].replace("0.8", str(opacity))
+    for src in data["data"][0]["link"]["source"]
+]
 
-# Format label as - <label> <total> 
-labels = data['data'][0]['node']['label']
-values = data['data'][0]['link']['value']
-targets = data['data'][0]['link']['target']
-sources = data['data'][0]['link']['source']
+# Format label as - <label> <total>
+labels = data["data"][0]["node"]["label"]
+values = data["data"][0]["link"]["value"]
+targets = data["data"][0]["link"]["target"]
+sources = data["data"][0]["link"]["source"]
 
-## Aggregate target_totals for each label
+# Aggregate target_totals for each label
 target_totals = {}
 for i, target in enumerate(targets):
     if target not in target_totals:
         target_totals[target] = values[i]
     else:
-        target_totals[target]+= values[i]
+        target_totals[target] += values[i]
 
-## Aggregate source_totals for each label
+# Aggregate source_totals for each label
 source_totals = {}
 for i, source in enumerate(sources):
     if source not in source_totals:
         source_totals[source] = values[i]
     else:
-        source_totals[source]+= values[i]
+        source_totals[source] += values[i]
 
-## Combine the label with the source_totals 
+# Combine the label with the source_totals
 for i, label in enumerate(labels):
     if i in target_totals:
         labels[i] = f"{labels[i]} {target_totals[i]}"
     else:
         labels[i] = f"{labels[i]} {source_totals[i]}"
 
-print("labels", labels)
-print("target_totals", target_totals)
-print("source_totals", source_totals)
-
 # Build Sankey diagram
-fig = go.Figure(data=[go.Sankey(
-    valueformat = ".0f",
-    # Define nodes
-    node = dict(
-      pad = 15,
-      thickness = 15,
-      line = dict(color = "black", width = 0.5),
-      label =  labels,
-      color =  data['data'][0]['node']['color']
-    ),
-    # Add links
-    link = dict(
-        source =  data['data'][0]['link']['source'],
-        target =  data['data'][0]['link']['target'],
-        value =  data['data'][0]['link']['value'],
-        label =  data['data'][0]['link']['label'],
-        color =  data['data'][0]['link']['color']
-))])
+fig = go.Figure(
+    data=[
+        go.Sankey(
+            valueformat=".0f",
+            # Define nodes
+            node=dict(
+                pad=15,
+                thickness=15,
+                line=dict(color="black", width=0.5),
+                label=labels,
+                color=data["data"][0]["node"]["color"],
+            ),
+            # Add links
+            link=dict(
+                source=data["data"][0]["link"]["source"],
+                target=data["data"][0]["link"]["target"],
+                value=data["data"][0]["link"]["value"],
+                label=data["data"][0]["link"]["label"],
+                color=data["data"][0]["link"]["color"],
+            ),
+        )
+    ]
+)
 
 fig.update_layout(
-    title_text="Craiglist Housing Search<br>1 bedroom, 1 bath",
-    font_size=10
+    title_text="Craiglist Apartment Hunt<br>June 1st, 2022 to July 1st 2022",
+    font_size=10,
 )
 
 # Show the diagram
